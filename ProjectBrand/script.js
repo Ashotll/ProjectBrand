4e220d4ffa9e087c2f35e5869b4ef38461ba8174
Ashotll <ashotstepanyan123@icloud.com>
2025-01-14 19:45:19 +0400

    Add files via upload

---
+++ ProjectBrand/script.js
@@ -0,0 +1,8 @@
+function myFunction() {
+  var x = document.getElementById("menu");
+  x.classList.toggle("toggle");
+  var z = document.getElementById("meun");
+  z.classList.toggle("toggle");
+  var a = document.getElementById("scrollblock");
+  a.classList.toggle("two");
+}
